rix1/46brew

FlowType: Replaced FlowTypes.js with global types

components/ColorButton.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { cx, css } from 'emotion';
 import { withTheme } from 'emotion-theming';
-import { type ThemeProps } from '../FlowTypes';
+
 
 type Props = {
   children: React$Node,
@@ -14,7 +14,7 @@ const ColorButton = ({
   className = '',
   theme,
   ...rest
-}: Props & ThemeProps = {}) => (
+}: Props & Brew$ThemeProps = {}) => (
   <button
     type="button"
     className={cx(

components/InlineInput.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { cx, css } from 'react-emotion';
 import { withTheme } from 'emotion-theming';
-import type { ThemeProps } from '../FlowTypes';
 
 type Props = { className?: string, error?: boolean };
 
@@ -11,7 +10,7 @@ const InlineInput = ({
   theme,
   error = false,
   ...rest
-}: Props & ThemeProps = {}) => (
+}: Props & Brew$ThemeProps = {}) => (
   <input
     className={cx(
       'ph2',

components/Line.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import styled, { css } from 'react-emotion';
 import { withTheme } from 'emotion-theming';
-import type { ThemeProps } from '../FlowTypes';
 
 type Props = {
   position: number,
@@ -22,7 +21,7 @@ const Line = ({
   contained = false,
   theme,
   ...rest
-}: Props & ThemeProps = {}) => {
+}: Props & Brew$ThemeProps = {}) => {
   if (contained) {
     return (
       <div

rix1/rix1-next-starter

components/ColorButton.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { cx, css } from 'emotion';
 import { withTheme } from 'emotion-theming';
-import { type ThemeProps } from '../FlowTypes';
+
 
 type Props = {
   children: React$Node,
@@ -14,7 +14,7 @@ const ColorButton = ({
   className = '',
   theme,
   ...rest
-}: Props & ThemeProps = {}) => (
+}: Props & App$ThemeProps = {}) => (
   <button
     type="button"
     className={cx(

components/InlineInput.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { cx, css } from 'react-emotion';
 import { withTheme } from 'emotion-theming';
-import type { ThemeProps } from '../FlowTypes';
 
 type Props = { className?: string, error?: boolean };
 
@@ -11,7 +10,7 @@ const InlineInput = ({
   theme,
   error = false,
   ...rest
-}: Props & ThemeProps = {}) => (
+}: Props & App$ThemeProps = {}) => (
   <input
     className={cx(
       'ph2',

components/Line.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import styled, { css } from 'react-emotion';
 import { withTheme } from 'emotion-theming';
-import type { ThemeProps } from '../FlowTypes';
 
 type Props = {
   position: number,
@@ -22,7 +21,7 @@ const Line = ({
   contained = false,
   theme,
   ...rest
-}: Props & ThemeProps = {}) => {
+}: Props & App$ThemeProps = {}) => {
   if (contained) {
     return (
       <div