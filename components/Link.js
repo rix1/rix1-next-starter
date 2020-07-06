9e08b32e78aa4f304d5704616b500b76201ecae2
Rikard Eide <rikardeide@gmail.com>
2020-07-06 22:02:11 +0200

    Fix eslint issue with Nextjs links

--- components/Link.js
+++ components/Link.js
@@ -2,19 +2,15 @@
 import React, { type Node } from 'react';
 import NextLink from 'next/link';
 
-type LinkProps = {
+type LinkProps = {|
   children: Node,
   href: string,
-};
+|};
 
-const Link = ({ children, href, ...rest }: LinkProps) => (
+const Link = ({ children, href }: LinkProps) => (
   <NextLink href={href}>
-    <a
-      className="f5 fw4 dim link near-black pv1"
-      // eslint-disable-next-line react/jsx-props-no-spreading
-      {...rest}>
-      {children}
-    </a>
+    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+    <a className="f5 fw4 dim link near-black pv1">{children}</a>
   </NextLink>
 );
 
